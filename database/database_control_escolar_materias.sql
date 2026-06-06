1d8da73c0b92879a120b1bda14ded19fef9dde34
Diego Lendeche <diegolendeche2@gmail.com>
2026-06-05 20:57:13 -0600

    Add files via upload

---
+++ database/database_control_escolar_materias.sql
@@ -0,0 +1,14 @@
+id_materia,nombre_materia,creditos,id_maestro
+1,"Base de Datos",5,1
+2,"Programación Web",5,2
+3,"Redes de Computadoras",5,3
+4,"Cálculo Integral",6,4
+5,"Programación Orientada a Objetos",5,5
+6,"Contabilidad Financiera",5,6
+7,"Derecho Mercantil",5,7
+8,"Marketing Digital",5,8
+9,"Estructura de Datos",5,5
+10,"Probabilidad y Estadística",5,4
+11,"Administración de Proyectos",4,8
+26,Matematicas,5,7
+28,Español,5,2
